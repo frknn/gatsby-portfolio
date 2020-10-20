a433505d6fa4eaac6bf5ee06f9f32b07b19fda00
frknn <frknstbs@gmail.com>
2020-10-20 18:22:40 +0300

    added contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import {FaLinkedin} from 'react-icons/fa'
-import {FaGithubSquare} from 'react-icons/fa'
-import {FaAt} from 'react-icons/fa'
+import { FaLinkedin } from 'react-icons/fa'
+import { FaGithubSquare } from 'react-icons/fa'
+import { FaAt } from 'react-icons/fa'
 
 const Contact = () => {
   return (
@@ -9,11 +9,17 @@ const Contact = () => {
       <div className="contact-content">
         <h1 className="contact-title">Contact Me</h1>
         <div className="contact-links">
-         <a href="https://www.linkedin.com/in/furkansetbasi/"><FaLinkedin className="contact-links__square-link"/></a>
-         <span className="contact-links__email">frknstbs<FaAt className="contact-links__at-symbol" />gmail.com</span>
-         <a href="https://github.com/frknn"><FaGithubSquare className="contact-links__square-link"/></a>
+          <a href="https://www.linkedin.com/in/furkansetbasi/"><FaLinkedin className="contact-links__square-link" /></a>
+          <span className="contact-links__email">frknstbs<FaAt className="contact-links__at-symbol" />gmail.com</span>
+          <a href="https://github.com/frknn"><FaGithubSquare className="contact-links__square-link" /></a>
         </div>
       </div>
+      <form name="message" method="POST" netlify>
+        <label htmlFor="Your message: ">
+          <input type="text" name="visitorMessage" />
+          <button type="submit">Send</button>
+        </label>
+      </form>
     </section>
   )
 }
